de25ab1544085fbe03f272cb357017d6a4e809a9
akodali18 <akodali@vmware.com>
2018-07-20 15:32:03 -0700

    Fixed unit tests

--- wflambda_test.go
+++ wflambda_test.go
@@ -1,17 +1,17 @@
 package wflambda
 
 import (
-	"os"
 	"testing"
+	"os"
 )
 
 func TestGetStandardLambdaMetricName(t *testing.T) {
-	actualName := getStandardLambdaMetricName("customMetric", true)
+	actualName := getStandardLambdaMetricName("customMetric" , true)
 	expectedName := "aws.lambda.wf.customMetric_event"
 	if actualName != expectedName {
 		t.Error("Metric names don't match ", expectedName, actualName)
 	}
-	actualName = getStandardLambdaMetricName("customMetrics", false)
+	actualName = getStandardLambdaMetricName("customMetrics" , false)
 	expectedName = "aws.lambda.wf.customMetrics"
 	if actualName != expectedName {
 		t.Error("Metric names don't match ", expectedName, actualName)
@@ -21,12 +21,12 @@ func TestGetStandardLambdaMetricName(t *testing.T) {
 func TestGetAndValidateLambdaEnvironment(t *testing.T) {
 	os.Setenv("WAVEFRONT_URL", "https://demo.wavefront.com")
 	os.Setenv("WAVEFRONT_API_TOKEN", "demo-api-token")
-	os.Setenv("IS_REPORT_STANDARD_METRICS", "False")
+ 	os.Setenv("REPORT_STANDARD_METRICS", "False")
 	expected := getAndValidateLambdaEnvironment()
 	if expected != false {
 		t.Error("Validate environmental variables failed ", expected, "False")
 	}
-	os.Setenv("IS_REPORT_STANDARD_METRICS", "true")
+	os.Setenv("REPORT_STANDARD_METRICS", "true")
 	expected = getAndValidateLambdaEnvironment()
 	if expected != true {
 		t.Error("Validate environmental variables failed ", expected, "true")
